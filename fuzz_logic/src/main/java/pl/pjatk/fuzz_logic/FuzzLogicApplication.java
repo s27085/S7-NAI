32459b3a01f759afdb2946bac784087bce59bcd3
Konrad <s27085@pjwstk.edu.pl>
2025-10-18 16:53:38 +0200

    module: java project project files for NAI_02 excercise

---
+++ fuzz_logic/src/main/java/pl/pjatk/fuzz_logic/FuzzLogicApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.fuzz_logic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuzzLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuzzLogicApplication.class, args);
+	}
+
+}
---
+++ fuzz_logic/src/test/java/pl/pjatk/fuzz_logic/FuzzLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.fuzz_logic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuzzLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
