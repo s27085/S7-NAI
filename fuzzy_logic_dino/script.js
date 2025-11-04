0bcd0a0154d15f94067f467c9eee34a5340326af
fabian.fetter <fabian.fetter@woodpecker.co>
2025-11-04 22:29:56 +0100

    feat: add duck

--- fuzzy_logic_dino/script.js
+++ fuzzy_logic_dino/script.js
@@ -8,19 +8,19 @@ logBox.style.bottom = "20px";
 document.body.appendChild(logBox);
 
 const distLogic = new Logic()
-  .init("veryClose", new ReverseGrade(0, 120))
-  .or("close", new Triangle(60, 100, 140))
-  .or("far", new Grade(120, 220));
+  .init("veryClose", new ReverseGrade(0, 70))
+  .or("close", new Triangle(70, 90, 100))
+  .or("far", new Grade(100, 200));
 
 const speedLogic = new Logic()
-  .init("slow", new Trapezoid(0, 0, 6, 8))
+  .init("slow", new ReverseGrade(0, 8))
   .or("medium", new Triangle(7, 9.5, 12))
   .or("fast", new Grade(11, 14));
 
-const gapLogic = new Logic()
-  .init("short", new Trapezoid(0, 0, 50, 100))
-  .or("medium", new Triangle(80, 150, 250))
-  .or("long", new Grade(250, 800));
+const obstacleHeightLogic = new Logic()
+  .init("low", new ReverseGrade(0, 60))
+  .or("medium", new Triangle(60, 70, 80))
+  .or("high", new Grade(80, 150));
 
 const game = () => {
   const tRexPos = runnerInstance.tRex.xPos;
@@ -29,25 +29,36 @@ const game = () => {
   const jump = () => runnerInstance.tRex.startJump(100);
 
   const dist = nextObstaclePos - tRexPos;
-  const gap = nextObstacle?.gap;
+  const obstacleHeight =
+    runnerInstance.horizon.dimensions.height - nextObstacle?.yPos;
   const speed = runnerInstance.currentSpeed;
 
   const defuzzifiedDist = distLogic.defuzzify(dist).defuzzified;
-  const defuzzifiedGap = gapLogic.defuzzify(gap).defuzzified;
+  const defuzzifiedObstacleHeight =
+    obstacleHeightLogic.defuzzify(obstacleHeight).defuzzified;
   const defuzzifiedSpeed = speedLogic.defuzzify(speed).defuzzified;
 
-  logBox.innerText = `Distance: ${defuzzifiedDist} | Gap: ${defuzzifiedGap} | Speed: ${defuzzifiedSpeed}`;
+  logBox.innerText = `Distance: ${defuzzifiedDist} (${dist}) | Obstacle Height: ${defuzzifiedObstacleHeight} (${obstacleHeight}) | Speed: ${defuzzifiedSpeed} (${speed})`;
 
   let action = "idle";
-
+  //Rules
   if (defuzzifiedDist === "veryClose") {
-    action = "jump";
-  } else if (defuzzifiedDist === "close" && defuzzifiedSpeed !== "slow") {
+    action = defuzzifiedObstacleHeight === "low" ? "jump" : "duck";
+  } else if (
+    defuzzifiedDist === "close" &&
+    defuzzifiedObstacleHeight === "low" &&
+    defuzzifiedSpeed !== "slow"
+  ) {
     action = "jump";
   }
 
   if (action === "jump") {
     jump();
+  } else if (action === "duck" && !runnerInstance.tRex.ducking) {
+    runnerInstance.tRex.setDuck(true);
+    setTimeout(() => {
+      runnerInstance.tRex.setDuck(false);
+    }, 200);
   }
 };
 
