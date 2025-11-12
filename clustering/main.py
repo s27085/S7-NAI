2e9126709c9b8c60f79eb56e31b7ad43c5b19a9b
Konrad <s27085@pjwstk.edu.pl>
2025-11-12 23:00:37 +0100

    refactor: change the directory, set person as key

--- clustering/main.py
+++ clustering/main.py
@@ -5,8 +5,22 @@ def extract_ratings(filename):
         if filename == "":
             raise ValueError
         datafile = pd.read_csv(filename, header=None)
-        data_array = datafile.to_dict("records")
-        return data_array
+        records_list = datafile.to_dict("records")
+        final_dict = {}
+        for record in records_list:
+            person_id = record.get(0) 
+            
+            if person_id:
+                ratings_data = {}
+                for key, value in record.items():
+                    if key != 0:
+                        try:
+                            ratings_data[key] = int(value)
+                        except (ValueError, TypeError):
+                            ratings_data[key] = value
+                
+                final_dict[person_id] = ratings_data
+        return final_dict
     except FileNotFoundError:
         print(f"File not found: {filename}")
         return []
@@ -21,3 +35,4 @@ if __name__ == "__main__":
     array = extract_ratings("ratings.csv")
     for i in array:
         print(f"{i}")
+        print(f"{array[i]}")
