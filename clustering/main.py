91e76edc873d44b89c390a846bdc9fa4401f215e
Konrad <s27085@pjwstk.edu.pl>
2025-11-12 22:53:35 +0100

    feat: add data reading from .csv with pandas

--- clustering/main.py
+++ clustering/main.py
@@ -1,5 +1,23 @@
-def extract_ratings():
-    return
+import pandas as pd
+
+def extract_ratings(filename):
+    try:
+        if filename == "":
+            raise ValueError
+        datafile = pd.read_csv(filename, header=None)
+        data_array = datafile.to_dict("records")
+        return data_array
+    except FileNotFoundError:
+        print(f"File not found: {filename}")
+        return []
+    except pd.errors.EmptyDataError:
+        print(f"The file {filename} is empty.")
+        return []
+    except ValueError:
+        print(f"Filename was not specified")
+        return []
 
 if __name__ == "__main__":
-    extract_ratings()
+    array = extract_ratings("ratings.csv")
+    for i in array:
+        print(f"{i}")
