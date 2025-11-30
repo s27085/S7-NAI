da9d6054f1f34b7a3ccf637d6bc4c16079577158
alphanoodle <fijalkowskikonras@gmail.com>
2025-11-30 22:48:02 +0100

    feat(SVM): linear kernel visualization

--- decision_tree_SVM/main.py
+++ decision_tree_SVM/main.py
@@ -7,6 +7,8 @@ import pandas as pd
 import scipy
 from sklearn import tree
 from sklearn import svm
+import numpy as np
+from sklearn.calibration import LabelEncoder
 
 MINE = 0
 ROCK = 1
@@ -22,13 +24,58 @@ def read_sonar_data():
 
 def visualize_decision_tree(prediction):
     
-    plt.figure(figsize=(20, 10))
+    plt.figure(figsize=(25, 10))
     tree.plot_tree(prediction, 
           filled=True,
           class_names=['Mine', 'Rock'],
           rounded=True,
           fontsize=10)
     
+    plt.show()
+
+def visualize_svm_decision_boundary(clf, sample_features, class_labels):
+
+    coefs = np.abs(clf.coef_[0])
+    top_2_indices = np.argsort(coefs)[-2:][::-1] 
+    i, j = top_2_indices[0], top_2_indices[1]
+
+    mean_all_features = sample_features.mean().values
+    
+    le = LabelEncoder()
+    y_numeric = le.fit_transform(class_labels)
+    PADDING = 0.05 
+    x_min, x_max = sample_features.iloc[:, i].min() - PADDING, sample_features.iloc[:, i].max() + PADDING
+    y_min, y_max = sample_features.iloc[:, j].min() - PADDING, sample_features.iloc[:, j].max() + PADDING
+    step_size = 0.01 
+    
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, step_size),
+                         np.arange(y_min, y_max, step_size))
+    grid_points_2D = np.c_[xx.ravel(), yy.ravel()]
+    N_grid_points = grid_points_2D.shape[0]
+    X_predict = np.tile(mean_all_features, (N_grid_points, 1))
+    X_predict[:, i] = grid_points_2D[:, 0] 
+    X_predict[:, j] = grid_points_2D[:, 1] 
+    
+    Z_string = clf.predict(X_predict) 
+    Z_numeric = le.transform(Z_string)
+    Z = Z_numeric.reshape(xx.shape)
+    
+    plt.figure(figsize=(8, 6))
+    
+    plt.contourf(xx, yy, Z, alpha=0.8, cmap=plt.cm.coolwarm)
+    
+    plt.scatter(
+        sample_features.iloc[:, i], 
+        sample_features.iloc[:, j], 
+        c=y_numeric, 
+        cmap=plt.cm.coolwarm, 
+        edgecolors='k', 
+        s=40
+    )
+    
+    plt.title(f"SVM (linear kernel) Decision Boundary (Slice at Mean: Features {i} vs {j})")
+    plt.xlabel(f"Feature {i}")
+    plt.ylabel(f"Feature {j}")
     plt.show()
 
 def SVM(sample_features, class_labels, visualize_tree, kernel_func='linear'):
@@ -38,6 +85,8 @@ def SVM(sample_features, class_labels, visualize_tree, kernel_func='linear'):
     single_row = sample_features.iloc[[117]]
     result = clf.predict_proba(single_row)
     print(f"SVM ({kernel_func} kernel) Predicted probabilities for first sample row: Mine: {result[0][MINE]*100:.2f}%, Rock: {result[0][ROCK]*100:.2f}%")
+    if kernel_func in ['linear']:
+        visualize_svm_decision_boundary(clf, sample_features, class_labels) if visualize_tree else None
 
 
 def decision_tree(sample_features, class_labels, visualize_tree):
