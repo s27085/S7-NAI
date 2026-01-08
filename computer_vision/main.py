ef799cf17682cd4fc141e279424b450bb53ca252
alphanoodle <fijalkowskikonras@gmail.com>
2026-01-08 22:14:11 +0100

    refactor: small tweaks

--- computer_vision/main.py
+++ computer_vision/main.py
@@ -16,19 +16,18 @@ import cv2
 import mediapipe as mp
 import math
 
-class MotionSensorShooter:
+class MotionDetector:
     def __init__(self):
-        self.source_stream = 'motion.MOV'
+        self.source_stream = 'motion.mov'
         self.previous_nose_position = None
         self.current_nose_position = None
         self.moving_threshold_pixels = 2
         self.draw_pose_landmarks = False
         self.frame = None
-        self.pose = mp.solutions.pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
         self.processed_pose = None
 
     def quit_on_keypress(self, key):
-        if cv2.waitKey(10) == ord(key):
+        if cv2.waitKey(20) == ord(key):
             exit()
 
     def draw_landmarks(self):
@@ -45,14 +44,14 @@ class MotionSensorShooter:
         cv2.putText(self.frame, text, (50, 100), font, 2, color, thickness)
         
 
-    def draw_crosshair_on_face(self, color=(0, 0, 255), thickness=2):
+    def draw_crosshair_on_face(self, color=(0, 0, 255), thickness=3):
         h, w, _ = self.frame.shape
         ear = self.processed_pose.pose_landmarks.landmark[7]
         ear_x, ear_y = int(ear.x * w), int(ear.y * h)
 
         nx, ny = self.current_nose_position
 
-        face_radius = int(math.dist((nx, ny), (ear_x, ear_y)) * 1.5)
+        face_radius = int(math.dist((nx, ny), (ear_x, ear_y)) * 2)
 
         cv2.circle(self.frame, (nx, ny), face_radius, color, thickness)
 
@@ -73,6 +72,7 @@ class MotionSensorShooter:
                 self.print_text_on_frame('HOLD', color=(0, 0, 255))
 
     def detect_motion(self):
+        pose = mp.solutions.pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
         cap = cv2.VideoCapture(self.source_stream)
 
         while cap.isOpened():
@@ -83,10 +83,10 @@ class MotionSensorShooter:
                 continue
 
             rgb_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
-            self.processed_pose = self.pose.process(rgb_frame)
+            self.processed_pose = pose.process(rgb_frame)
+            frame_height, frame_width, _ = self.frame.shape
 
             if self.processed_pose.pose_landmarks:
-                frame_height, frame_width, _ = self.frame.shape
 
                 nose_landmark = self.processed_pose.pose_landmarks.landmark[0]
                 nose_x, nose_y = int(nose_landmark.x * frame_width), int(nose_landmark.y * frame_height)
@@ -110,5 +110,5 @@ class MotionSensorShooter:
 
 
 if __name__ == "__main__":
-    cv = MotionSensorShooter()
+    cv = MotionDetector()
     cv.detect_motion()
